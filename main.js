593725f77362102f6c601bf6eb57fcd84ea7d9db
santiago <santiagoneira52@gmail.com>
2022-07-24 14:12:41 -0300

    listando talles, detalles, intentando mostrar los colores

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ const app = Vue.createApp({
   data: function () {
     return {
       product: "Socks",
-      description: "championes originales, muy buenos para realizar deporte.",
+      description: "Medias originales, muy buenas para realizar deporte.",
+      image: "./assets/images/socks_blue.jpg",
+      url: "https://picsum.photos/200",
+      stock: true,
+      oferta: true,
+      inventory: {
+        socks: 5,
+      },
+      detalles: ["100% algodón", "30% lana", "20% polyester"],
+      colores: [
+        { id: 1, color: "red" },
+        { id: 2, color: "blue" },
+      ],
+      talles: ["S", "M", "L", "XL"],
     };
   },
 });
